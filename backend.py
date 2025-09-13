0f82971648ff65dcec6bd39f066c50259c69766a
Lopes <Enzopaula05@gmail.com>
2025-09-12 21:51:19 -0300

    att

--- backend.py
+++ backend.py
@@ -62,4 +62,5 @@ def listar_pessoas():
     return jsonify(pessoas)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
